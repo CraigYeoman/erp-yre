134f1c704f0edeb19af3e27f50e707e89e0414f1
CraigYeoman <yeomancraig@gmail.com>
2023-01-29 13:41:11 -0600

    Basic Styling parts list

--- client/src/components/parts/PartsList.jsx
+++ client/src/components/parts/PartsList.jsx
@@ -23,18 +23,18 @@ const PartsList = () => {
   }
 
   return (
-    <div>
+    <div className="parts-list-container">
       {data.parts.map((part) => {
         const { name, part_number, manufacture, vendor, customer_price, _id } =
           part;
         return (
-          <div key={_id}>
+          <div className="parts-container" key={_id}>
             <Link onClick={() => selectPartID(_id)} to={`/partdetail/${_id}`}>
               {name}
             </Link>
             <p>{part_number}</p>
             <p>{manufacture}</p>
-            <p>{customer_price}</p>
+            <p>${customer_price}</p>
             <Link
               onClick={() => selectVendorID(vendor._id)}
               to={`/vendordetail/${vendor._id}`}

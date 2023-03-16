74cdc081448155154bc6f660a5e17b048ed3eeb4
CraigYeoman <yeomancraig@gmail.com>
2023-03-16 07:13:03 -0500

    Final styling for  Vendor List

--- client/src/components/vendor/VendorList.jsx
+++ client/src/components/vendor/VendorList.jsx
@@ -1,18 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../../context";
+import { Box, useTheme, Link } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "../Header";
 
 const VendorList = () => {
-  const { listType, selectVendorID, formatPhoneNumber } = useGlobalContext();
+  const { listType, selectVendorID, formatPhoneNumber, setLoading, loading } =
+    useGlobalContext();
   const [data, setData] = useState([{}]);
+  const theme = useTheme();
 
   useEffect(() => {
+    setLoading(true);
     fetch(`/api/v1/erp/${listType}`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
+        setLoading(false);
       });
   }, []);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1.5,
+      renderCell: (params) => (
+        <Link
+          component={RouterLink}
+          color="inherit"
+          onClick={() => selectVendorID(params.row._id)}
+          to={`/vendordetail/${params.row._id}`}
+        >
+          {params.row._id}
+        </Link>
+      ),
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+    },
+    {
+      field: "main_contact",
+      headerName: "Main Contact",
+      flex: 1,
+    },
+    {
+      field: "phone_number",
+      headerName: "Phone Number",
+      flex: 1,
+      renderCell: (params) => {
+        return (
+          <div className="rowitem">
+            {formatPhoneNumber(params.row.phone_number)}
+          </div>
+        );
+      },
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+  ];
 
   if (!data.vendors) {
     return (
@@ -22,24 +74,44 @@ const VendorList = () => {
     );
   }
   return (
-    <div className="vendor-list-container">
-      {data.vendors.map((vendor) => {
-        const { _id, name, main_contact, phone_number, email } = vendor;
-        return (
-          <div className="vendor-container" key={_id}>
-            <Link
-              onClick={() => selectVendorID(_id)}
-              to={`/vendordetail/${_id}`}
-            >
-              {name}
-            </Link>
-            <p>{main_contact}</p>
-            <p>{formatPhoneNumber(phone_number)}</p>
-            <p>{email}</p>
-          </div>
-        );
-      })}
-    </div>
+    <Box m="1.5rem 2.5rem">
+      <Header title="Parts" subtitle="List of Parts" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={loading || !data.vendors}
+          rows={data.vendors}
+          getRowId={(row) => row._id}
+          columns={columns}
+        />
+      </Box>
+    </Box>
   );
 };
 

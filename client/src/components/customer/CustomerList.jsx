e27fc1c587872dd9279ea0d6128e496ba27b379e
CraigYeoman <yeomancraig@gmail.com>
2023-03-17 06:37:01 -0500

    Final Styling - Job Types List & Customer List

--- client/src/components/customer/CustomerList.jsx
+++ client/src/components/customer/CustomerList.jsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../../context";
+import { Box, useTheme, Link } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "../Header";
 
 const CustomerList = () => {
-  const { selectCustomerID, listType, formatPhoneNumber } = useGlobalContext();
+  const { selectCustomerID, listType, formatPhoneNumber, setLoading, loading } =
+    useGlobalContext();
   const [data, setData] = useState([{}]);
+  const theme = useTheme();
 
   useEffect(() => {
+    setLoading(true);
     fetch(`/api/v1/erp/${listType}`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
+        setLoading(false);
       });
   }, []);
 
@@ -22,42 +29,110 @@ const CustomerList = () => {
     );
   }
 
-  return (
-    <div className="customer-list-container">
-      {data.customers.map((customer) => {
-        const {
-          _id,
-          first_name,
-          last_name,
-          phone_number,
-          email,
-          address_line_1,
-          address_line_2,
-          city,
-          state,
-          zip_code,
-        } = customer;
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1.5,
+      renderCell: (params) => (
+        <Link
+          component={RouterLink}
+          color="inherit"
+          onClick={() => selectCustomerID(params.row._id)}
+          to={`/customerdetail/${params.row._id}`}
+        >
+          {params.row._id}
+        </Link>
+      ),
+    },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      flex: 1,
+    },
+    {
+      field: "last_name",
+      headerName: "Last Name",
+      flex: 1,
+    },
+    {
+      field: "phone_number",
+      headerName: "Phone Number",
+      flex: 1,
+      renderCell: (params) => {
         return (
-          <div className="customer-container" key={_id}>
-            <fieldset>
-              <legend>
-                <Link
-                  onClick={() => selectCustomerID(_id)}
-                  to={`/customerdetail/${customer._id}`}
-                >
-                  {first_name} {last_name}
-                </Link>
-              </legend>
-              <p>{formatPhoneNumber(phone_number)}</p>
-              <p>{email}</p>
-              <p>
-                {address_line_1}, {city}, {state} {zip_code}
-              </p>
-            </fieldset>
+          <div className="rowitem">
+            {formatPhoneNumber(params.row.phone_number)}
           </div>
         );
-      })}
-    </div>
+      },
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address_line_1",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "state",
+      headerName: "State",
+      flex: 1,
+    },
+    {
+      field: "zip_code",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Customers" subtitle="List of Customers" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={loading || !data.customers}
+          rows={data.customers}
+          getRowId={(row) => row._id}
+          columns={columns}
+        />
+      </Box>
+    </Box>
   );
 };
 
--- client/src/components/jobtype/JobTypeList.jsx
+++ client/src/components/jobtype/JobTypeList.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../../context";
+import { Box, useTheme, Link } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "../Header";
 
 const JobTypeList = () => {
-  const { selectJobTypeID, listType } = useGlobalContext();
+  const { selectJobTypeID, listType, setLoading, loading } = useGlobalContext();
   const [data, setData] = useState([{}]);
+  const theme = useTheme();
 
   useEffect(() => {
+    setLoading(true);
     fetch(`/api/v1/erp/${listType}`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
+        setLoading(false);
       });
   }, []);
 
@@ -22,23 +28,68 @@ const JobTypeList = () => {
     );
   }
 
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1.5,
+      renderCell: (params) => (
+        <Link
+          component={RouterLink}
+          color="inherit"
+          onClick={() => selectJobTypeID(params.row._id)}
+          to={`/jobtypedetail/${params.row._id}`}
+        >
+          {params.row._id}
+        </Link>
+      ),
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+    },
+  ];
+
   return (
-    <div className="job-list-container">
-      <h3>Job Types</h3>
-      {data.jobTypes.map((jobType) => {
-        const { name, _id } = jobType;
-        return (
-          <div className="job-container" key={_id}>
-            <Link
-              onClick={() => selectJobTypeID(_id)}
-              to={`/jobtypedetail/${_id}`}
-            >
-              {name}
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <Box m="1.5rem 2.5rem">
+      <Header title="Job Types" subtitle="List of Job Types" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={loading || !data.jobTypes}
+          rows={data.jobTypes}
+          getRowId={(row) => row._id}
+          columns={columns}
+        />
+      </Box>
+    </Box>
   );
 };
 
